2c5f86828a3a608e7579fbbf0963c3a1dae2bc2c
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-04 15:15:13 +0200

    created component ui

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -5,7 +5,24 @@ export interface SearchProps {
 }
 
 const Search: React.FC< SearchProps > = props => {
-    return <input className={styles.input} type="text" name="search" placeholder="Search" />
+    return (
+        <form>
+            <input className={styles.input}
+                      type="text"
+                      name="search"
+                      placeholder="Search"
+                      autoComplete="off"
+            />
+            <input type="button"
+                   className={styles.buttonCancel}
+                   onClick={()=>{console.log("elo")}}
+            />
+            <input type="button"
+                   className={styles.buttonSearch}
+                   onClick={()=>{console.log("elo")}}
+            />
+        </form>
+)
 };
 
 export default Search;
