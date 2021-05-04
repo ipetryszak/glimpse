0bd3c61fa384e6d2e38c2e22fcb4041dde8f5858
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-04 21:56:35 +0200

    implemented universal select ui and logic

--- src/components/universal-select/universal-select.tsx
+++ src/components/universal-select/universal-select.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
-import styles from './search.module.scss';
-import {useState} from "react";
+import styles from './universal-select.module.scss';
 
 export interface UniversalSelectProps {
-
+    options: string[],
+    onSelect: Function
 }
 
-const UniversalSelect: React.FC< UniversalSelectProps > = props => {
+const UniversalSelect: React.FC< UniversalSelectProps > = ({options, onSelect}) => {
 
-    return <h1>Universal Select</h1>
+    return (
+        <select className={styles.select} onChange={ (e) => { onSelect(e.target.value) } }>
+            {
+                !!options.length && (
+                    options.map(
+                        (option: string, index: number) => <option value={option}
+                                                                   className={styles.option}
+                                                                   key={index} >{option}</option>
+                    )
+                )
+            }
+        </select>
+    )
 };
 
 export default UniversalSelect;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Search from "../../components/search";
+import UniversalSelect from "../../components/universal-select";
 
 export interface HomeProps {
 }
@@ -9,7 +10,8 @@ const Home: React.FC< HomeProps > = props => {
     return (
         <div>
             <h1>Home</h1>
-            <Search onSubmit={(search: any)=>{console.log(search)}} history={exampleHistory}/>
+            <Search onSubmit={(search: any)=>{console.log(search)}} history={exampleHistory} />
+            <UniversalSelect options={['YouTube', 'Vimeo']} onSelect={(e: any) => {console.log(e)}} />
         </div>
     );
 };
