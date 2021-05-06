de18655b5bf00d60257011548db6146e4fecd293
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-06 19:59:57 +0200

    home page basic setup

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 
+import styles from './home.module.scss'
+import VideoTile from "../../components/video-tile";
+
 export interface HomeProps {
 }
 
 const Home: React.FC< HomeProps > = props => {
 
-    return (<h1>Home</h1>);
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.header}>Trending now</h1>
+            <main>
+                <VideoTile />
+            </main>
+        </div>
+
+    );
 };
 
 export default Home;
