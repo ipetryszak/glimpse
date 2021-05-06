e68267c0a5885c8a5106a4085d6d51fcd61310ac
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-06 17:35:53 +0200

    linter fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -24,6 +24,7 @@ const Header: React.FC = () => {
 
     useEffect( () => {
         dispatch(fetchPopular());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     },[]);
 
     const handleSelect = ( option: VideoPlatforms) => {
